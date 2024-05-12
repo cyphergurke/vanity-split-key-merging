fccc2aa125801ddd1cbad19e8c2586a8f56bec2d
cyphergurke <cyberzuchini@protonmail.com>
2024-05-12 21:34:17 +0200

    add root route, change app title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import KeymergingComponent from "@/components/KeymergingComponent";
 
 
-export default function Page({params}: {params: {slug: any}}) {
+export default function Home() {
   return (
     <main className="flex h-[100dvh] w-[100dvw] bg-main justify-center items-center">
       <div className='w-[90%] md:w-5/6 lg:w-1/3 sm:w-2/3'>
-        <KeymergingComponent  vaddress={params.slug[0]} partialkey={params.slug[1]}/>
+        <KeymergingComponent />
       </div>
     </main>
   )
